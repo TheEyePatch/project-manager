9bba5e64986083749ea17954173a8622abb89561
Leif Bersamina <leifbersamina@gmail.com>
2022-08-01 00:00:18 +0800

    setup react on app

---
+++ app/javascript/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "../components/Home";
+
+
+function App(){
+  return <h1>APP</h1>
+}
+
+
+export default App;
