9d49a93df3275c533712fe47f6b7d66ed77628a0
Leif Bersamina <leifbersamina@gmail.com>
2022-08-02 22:35:29 +0800

    add SPA routing

---
+++ app/javascript/routes/AppRoutes.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "../components/Home";
+import About from "../components/About";
+
+
+function AppRoutes(){
+  return (
+    <Router>
+        <div className="App">
+         <ul className="App-header">
+           <li>
+             <Link to="/">Home</Link>
+           </li>
+           <li>
+             <Link to="/about">About Us</Link>
+           </li>
+         </ul>
+        <Routes>
+              <Route exact path='/' element={< Home />}></Route>
+              <Route exact path='/about' element={< About />}></Route>
+       </Routes>
+       </div>
+    </Router>
+  )
+}
+
+
+export default AppRoutes;
--- app/javascript/src/App.js
+++ app/javascript/src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "../components/Home";
+import AppRoutes from '../routes/AppRoutes'
 
 
 function App(){
-  return <h1>APP</h1>
+  return <AppRoutes/>;
 }
 
 
