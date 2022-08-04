1a00508b4ddc65af87bc6af9961a0bcb0893f006
Leif Bersamina <leifbersamina@gmail.com>
2022-08-04 23:05:53 +0800

    create basic user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_04_142244) do
+ActiveRecord::Schema.define(version: 2022_08_04_145235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,14 @@ ActiveRecord::Schema.define(version: 2022_08_04_142244) do
     t.bigint "project_id"
     t.bigint "user_id"
     t.bigint "sprint_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "account"
+    t.string "name"
+    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
