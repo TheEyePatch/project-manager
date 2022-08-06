202eec149ee0560772c984daa3d45bd05883eef2
Leif Bersamina <leifbersamina@gmail.com>
2022-08-06 22:07:06 +0800

    add validation for task spec

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ApplicationRecord
   belongs_to :board, optional: true
+  validates :title, presence: true
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
+  let(:project) { Project.create(name: 'Project Name', description: 'Description', owner_id: 1) }
   context 'create' do
     it 'should not be valid without title' do
       expect(Task.new).not_to be_valid
+    end
+
+    it 'should valid with correct params' do
+      task = Task.new(title: 'Task Title', description: 'Description', project: project)
+      expect(task).to be_valid
     end
   end
 end
