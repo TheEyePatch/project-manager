b8dbe5558f7f2e80598c0d52c258588961522d0f
Leif Bersamina <leifbersamina@gmail.com>
2022-08-09 23:26:08 +0800

    add basic create method on user registration

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,3 +1,16 @@
 class Api::V1::RegistrationsController < Api::ApiController
-  def create; end
+  def create
+    user = User.new(registrations_params)
+
+    return unless user.save
+    render json: {
+      user_params: registrations_params,
+    }, status: :ok
+  end
+
+  private
+
+  def registrations_params
+    params.permit(:account, :name, :password)
+  end
 end
