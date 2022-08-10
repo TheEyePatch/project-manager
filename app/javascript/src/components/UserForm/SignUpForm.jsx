c77970e25bff212e4ac0a67e1e4551ce780fdc72
Leif Bersamina <leifbersamina@gmail.com>
2022-08-10 23:23:25 +0800

    add basic form inputs on SignUpForm component

--- app/javascript/src/components/UserForm/SignUpForm.jsx
+++ app/javascript/src/components/UserForm/SignUpForm.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
-import { Paper, Grid, Avatar } from '@mui/material'
+import { Paper, Grid, Avatar, TextField, Button } from '@mui/material'
 
 function SignUpForm(){
   const style = { padding: '30px 20px', width: '300px', margin: '20px auto' }
   return (
     <Grid>
-      <Paper elevation={8} variant='outlined' style={style}>
+      <Paper variant='outlined' style={style}>
         <Avatar/>
         <h1>Sign Up</h1>
+        <div>
+          <form>
+            <TextField fullWidth required id="email" label="Email" variant="standard" margin="dense" />
+            <TextField fullWidth required id="name" label="Name" variant="standard"  margin="dense"/>
+            <TextField fullWidth required id="password" label="Password" variant="standard" type="password" margin="dense"/>
+            <Button type='submit' variant="contained" color='primary'>Sign Up</Button>
+          </form>
+        </div>
       </ Paper>
     </Grid>
   )
