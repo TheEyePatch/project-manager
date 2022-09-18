2b3832f4d5d444a26c1b34feed481d4c08464d4b
Leif Bersamina <leifbersamina@gmail.com>
2022-09-18 20:19:25 +0800

    add ThemeProvider for custom MUI thems

--- app/javascript/src/App.js
+++ app/javascript/src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
 import AppRoutes from './routes/AppRoutes'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
-
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#5e17eb'
+    }
+  }
+})
 function App(){
-  return <AppRoutes/>;
+  return (
+    <ThemeProvider theme={theme}>
+      <AppRoutes/>
+    </ThemeProvider>
+  );
 }
 
 
