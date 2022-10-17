2fa76fc8c190d32c31d9555bb6fe3d6c565a508e
Leif Bersamina <leifbersamina@gmail.com>
2022-10-17 19:41:24 +0800

    move project form to projects component

--- app/javascript/src/components/Projects/Projects.jsx
+++ app/javascript/src/components/Projects/Projects.jsx
@@ -1,9 +1,9 @@
 import React, { useContext, useEffect, useState } from 'react';
 import AuthContext from '../../store/AuthContext';
 import { getProjects } from '../../api/index';
-import { Project } from './../index';
+import { Project, ProjectForm } from './../index';
 import Grid from '@mui/material/Grid';
-import { Container } from '@mui/material'
+import { Button, Container } from '@mui/material';
 
 
 function Projects(){
@@ -11,6 +11,8 @@ function Projects(){
   // const userLoggedIn = authCtx.loggedIn;
   const token = authCtx.token;
   const [projects, setProjects] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false)
+  const handleNewProject = () => setModalOpen(true);
 
   useEffect(() => {
     getProjects({token: token})
@@ -21,15 +23,28 @@ function Projects(){
   }, []);
 
   return (
-    <Grid container spacing={2}>
-      {projects?.map((project) => {
-        return (
-          <Grid item xs={12} sm={6} md={3} key={project.id}>
-            <Project project={project} />
-          </Grid>
-          )
-      })}
-    </Grid>
+    <Container>
+      <div style={{ 
+        margin: '1rem 0 1rem',
+        display: 'flex',
+        justifyContent: 'flex-end'
+      }}>
+        <Button variant="contained" size="large" onClick={handleNewProject}>
+          New Project
+        </Button>
+        <ProjectForm modalOpen={modalOpen} setModalOpen={setModalOpen} setProjects={setProjects}/>
+      </div>
+
+      <Grid container spacing={2}>
+        {projects?.map((project) => {
+          return (
+            <Grid item xs={12} sm={6} md={3} key={project.id}>
+              <Project project={project} />
+            </Grid>
+            )
+        })}
+      </Grid>
+    </Container>
     )
 }
 
--- app/javascript/src/pages/ProjectsPage.jsx
+++ app/javascript/src/pages/ProjectsPage.jsx
@@ -1,30 +1,15 @@
 import React, { useState } from "react";
-import { Button, Container } from '@mui/material';
-import { Projects, ProjectForm } from './../components/index';
+import { Projects } from './../components/index';
 
 function ProjectsPage() {
-  const [modalOpen, setModalOpen] = useState(false)
-  const handleNewProject = () => setModalOpen(true);
 
   return (
-    <Container>
     <section style={{
       paddingTop: '1rem',
       paddingTop: '1rem'
     }}>
-      <div style={{ 
-        margin: '1rem 0 1rem',
-        display: 'flex',
-        justifyContent: 'flex-end'
-      }}>
-        <Button variant="contained" size="large" onClick={handleNewProject}>
-          New Project
-        </Button>
-        <ProjectForm modalOpen={modalOpen} setModalOpen={setModalOpen} />
-      </div>
       <Projects/>
     </section>
-  </Container>
   )
 }
 
