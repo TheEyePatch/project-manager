c0b9228fb4aaca427c7b8d5385b4b0a242dffc00
JVD-Leif <l.bersamina@jvd.com.ph>
2023-05-25 16:19:17 +0800

    Add comments controller
    Adds comments controller with rspec request tests. Includes an image file in support/assets for
    image attachment test for rich text editor, as well as small adjust for dockerignore on dev environment.

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,65 @@
+class Api::V1::CommentsController <  Api::ApiController
+  before_action :authenticate_user, only: %i[create update]
+
+  PAGE_LIMIT = 5
+
+  def index
+    comments =
+      task.comments
+          .with_last_comment_id(params[:last_comment_id])
+          .limit(PAGE_LIMIT)
+          .order(id: :desc)
+
+    render json: {
+      comments: comments
+    }, status: :ok
+  end
+
+  def create
+    comment =
+      task.comments
+          .build(comments_params.merge({user_id: current_user.id}))
+
+    if comment.valid?
+      render json: {
+        comment: comment,
+      }, status: :ok
+    else
+      render json: {}, status: :bad_request
+    end
+  end
+
+  def update
+    comment = Comment.find(params[:id])
+
+    if comment.update(comments_params)
+      render json: {
+        comment: comment,
+      }, status: :ok
+    else
+      render json: {}, status: :bad_request
+    end
+  end
+
+  def upload_image
+    comment = Comment.find(params[:id])
+
+    if params[:image].present?
+      comment.images.attach(params[:image]) 
+      render json: {
+        image_url: rails_blob_path(comment.images.last),
+        attachment_id: comment.images.last.id
+      }
+    end
+  end
+
+  private
+
+  def task
+    @task ||= Task.find(params[:task_id])
+  end
+
+  def comments_params
+    params.require(:comment).permit(:body)
+  end
+end
