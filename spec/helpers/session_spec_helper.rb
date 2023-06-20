c114c36a8a3a883eaed77b2537d8effc74b680ff
JVD-Leif <l.bersamina@jvd.com.ph>
2023-06-20 17:28:19 +0800

    Adjust profile_spec and registrations_controller
    This changes' main focus is to modify profile_spec to be more readable and semantically
    correct as well as adding new tests to increase confidence.
    
    The changes also includes adjusting the error handling of 'update' in registrations,
     validation of user model and adding a session spec helper method.

--- spec/helpers/session_spec_helper.rb
+++ spec/helpers/session_spec_helper.rb
@@ -31,4 +31,31 @@ module SessionSpecHelper
       headers: { 'Content-Type' => 'application/json' })
     end
   end
+
+  def patch_user(user = nil, &block)
+    if block_given?
+      patch('/api/v1/user',
+        params: { user: yield }.to_json,
+        headers: {
+          'Content-Type' => 'application/json',
+          'Authorization' => @token || '123456',
+        },
+      )
+    else
+      patch('/api/v1/user',
+        params: {
+          user: {
+            email: user.email,
+            account: user.account,
+            first_name: user.first_name,
+            last_name: user.last_name,
+          },
+        }.to_json,
+        headers: {
+          'Content-Type' => 'application/json',
+          'Authorization' => @token || '123456',
+        },
+      )
+    end
+  end
 end
--- spec/requests/profile_spec.rb
+++ spec/requests/profile_spec.rb
@@ -1,68 +1,124 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+  let(:user) { create(:user) }
+
   describe 'GET user profile' do
-    let(:user) { create(:user) }
+    context 'when user is logged in' do
+      before do
+        sign_in(user)
+        @token = response_body[:token]
+      end
 
-    before do
-      post('/api/v1/sign_in',
-        params: {
-          sign_in: {
-            email: user.email,
-            password: user.password
-          }
-        }.to_json,
-        headers: { 'Content-Type': 'application/json' }
-      )
+      it 'returns success user profile response' do
+        get('/api/v1/profile', headers: { 'Authorization' => @token })
 
-      @token = response_body[:token]
+        expect(response_body[:account]).to eql(user.account)
+      end
     end
 
-    it 'returns success user profile response' do
-      get('/api/v1/profile', headers: { 'Authorization' => @token })
-      expect(response_body[:account]).to eql(user.account)
-      expect(response_body[:account]).to eql(user.account)
-      expect(response_body[:account]).to eql(user.account)
+    context 'when user token is invalid' do
+      it 'returns failed response' do
+        get('/api/v1/profile', headers: { 'Authorization' => '123456' })
+
+        expect(response).to have_http_status(401)
+        expect(response_body[:errors].map(&:downcase)).to include('invalid token')
+      end
     end
   end
 
   describe 'PUT/PATCH user profile' do
-    let(:user) { create(:user) }
+    context 'when user is logged in' do
+      before do
+        sign_in(user)
+        @token = response_body[:token]
 
-    before do
-      post('/api/v1/sign_in',
-        params: {
-          sign_in: {
-            email: user.email,
-            password: user.password
-          }
-        }.to_json,
-        headers: { 'Content-Type': 'application/json' }
-      )
-
-      @token = response_body[:token]
-    end
-
-    it 'returns success with correct params' do
-      patch('/api/v1/user',
-        params: {
-          user: {
+        patch_user do
+          {
             email: 'newEmail@email.com',
             account: 'newAccount',
             first_name: 'newFirstName',
-            last_name: 'newLastName'
+            last_name: 'newLastName',
           }
-        }.to_json,
-        headers: {
-          Authorization: @token,
-          'Content-Type': 'application/json'
-        }
-      )
+        end
+      end
 
-      expect(response_body[:email]).to eql('newEmail@email.com')
-      expect(response_body[:account]).to eql('newAccount')
-      expect(response_body[:first_name]).to eql('newFirstName')
-      expect(response_body[:last_name]).to eql('newLastName')
+      context 'with valid params' do
+        it 'returns success with correct params' do
+          expect(response_body[:email]).to eql('newEmail@email.com')
+          expect(response_body[:account]).to eql('newAccount')
+          expect(response_body[:first_name]).to eql('newFirstName')
+          expect(response_body[:last_name]).to eql('newLastName')
+        end
+      end
+
+      context 'with invalid params' do
+        it 'returns invalid email' do
+          patch_user { { email: 'leifemail.com' } }
+
+          expect(response).to have_http_status(400)
+          expect(response_body.dig(:errors, :email)).to be_present
+          expect(response_body.dig(:errors, :email)).to include('invalid format')
+        end
+
+        it 'returns invalid account' do
+          patch_user { { account: 'le' } }
+
+          expect(response).to have_http_status(400)
+          expect(response_body.dig(:errors, :account)).to be_present
+          expect(response_body.dig(:errors, :account)).to include('invalid character length (min: 3, max: 35)')
+        end
+
+        it 'returns missing email' do
+          patch_user { { email: '' } }
+
+          expect(response).to have_http_status(400)
+          expect(response_body.dig(:errors, :email)).to be_present
+          expect(response_body.dig(:errors, :email)).to include("can't be blank")
+        end
+
+        it 'returns missing account' do
+          patch_user { { account: '' } }
+
+          expect(response).to have_http_status(400)
+          expect(response_body.dig(:errors, :account)).to be_present
+          expect(response_body.dig(:errors, :account)).to include("can't be blank")
+        end
+
+        it 'returns missing first_name' do
+          patch_user { { first_name: '' } }
+
+          expect(response).to have_http_status(400)
+          expect(response_body.dig(:errors, :first_name)).to be_present
+          expect(response_body.dig(:errors, :first_name)).to include("can't be blank")
+        end
+
+        it 'returns missing last_name' do
+          patch_user { { last_name: '' } }
+
+          expect(response).to have_http_status(400)
+          expect(response_body.dig(:errors, :last_name)).to be_present
+          expect(response_body.dig(:errors, :last_name)).to include("can't be blank")
+        end
+      end
+    end
+
+    context 'when user token is invalid' do
+      before do
+        patch_user do
+          {
+            email: 'newEmail@email.com',
+            account: 'newAccount',
+            first_name: 'newFirstName',
+            last_name: 'newLastName',
+          }
+        end
+      end
+
+      it 'returns failed auth response' do
+        expect(response).to have_http_status(401)
+        expect(response_body[:errors].map(&:downcase)).to include('invalid token')
+      end
     end
   end
 end
